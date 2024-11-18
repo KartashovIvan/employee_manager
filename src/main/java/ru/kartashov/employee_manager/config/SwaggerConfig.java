8f0b0d688d19fd39838ca55d7619a87d221fd04e
KartashovIvan <vano.noname69@yandex.ru>
2024-11-18 15:09:55 +0300

    Исправлен конфиг Swagger #2

--- src/main/java/ru/kartashov/employee_manager/config/SwaggerConfig.java
+++ src/main/java/ru/kartashov/employee_manager/config/SwaggerConfig.java
@@ -2,30 +2,19 @@ package ru.kartashov.employee_manager.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
-import lombok.AllArgsConstructor;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
-
 @Configuration
-@AllArgsConstructor
 public class SwaggerConfig {
-
-    private ServerProperties serverProperties;
 
     @Bean
     public OpenAPI init() {
-        Server afishaServer = new Server();
-        afishaServer.setUrl("http://localhost:%s".formatted(serverProperties.getPort()));
-
         Info info = new Info();
         info.title("Employee manager");
         info.version("1.0");
         info.description("Сервис по управлению списком сотрудников");
 
-        return new OpenAPI().info(info).servers(List.of(afishaServer));
+        return new OpenAPI().info(info);
     }
 }
