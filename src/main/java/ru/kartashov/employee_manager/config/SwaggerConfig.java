f226895725aeabed9fd016a50fa57f548db957fc
KartashovIvan <vano.noname69@yandex.ru>
2024-11-18 13:59:40 +0300

    Исправлен конфиг Swagger

--- src/main/java/ru/kartashov/employee_manager/config/SwaggerConfig.java
+++ src/main/java/ru/kartashov/employee_manager/config/SwaggerConfig.java
@@ -3,18 +3,23 @@ package ru.kartashov.employee_manager.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
 @Configuration
+@AllArgsConstructor
 public class SwaggerConfig {
+
+    private ServerProperties serverProperties;
 
     @Bean
     public OpenAPI init() {
         Server afishaServer = new Server();
-        afishaServer.setUrl("http://localhost:8080");
+        afishaServer.setUrl("http://localhost:%s".formatted(serverProperties.getPort()));
 
         Info info = new Info();
         info.title("Employee manager");
